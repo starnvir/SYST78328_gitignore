48282f6e4dca672eae824cf38bcd5403d1b68644
starnvir <sitarnvi@sheridancollege.ca>
2022-01-26 13:37:56 -0500

    First commit

---
+++ src/shape/Shape.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shape;
+
+/**
+ *
+ * @author sangh
+ */
+public class Shape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Square sq = new Square();
+    }
+    
+}
